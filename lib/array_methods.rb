5d9c15dee4c7222a6a35168cb1d5223a9e03507e
Austin Trow <austintrow@gmail.com>
2019-07-01 17:05:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,12 +20,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min = 
+  min = array[0]
   array.length.times do |index|
-    if array[index] > max
-      max = array[index]
+    if array[index] < min
+      min = array[index]
     end
   end
   
-  max
+  min
 end
